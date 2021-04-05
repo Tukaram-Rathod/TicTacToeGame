6e3921b0a0830bfe662b1ba81ddcb41e3c7e746a
Tukaram-Rathod <tukaram.p.rathod02@gmail.com>
2021-04-05 14:11:29 +0530

    Added. created board in uc 1

--- src/game/tictactoe/TicTacToeGame.java
+++ src/game/tictactoe/TicTacToeGame.java
@@ -2,17 +2,15 @@ package game.tictactoe;
 
 public class TicTacToeGame {
 	
-	public void  board() {
+	public static void  board() {
 		char [] board = new char[10];
+		System.out.println("Welcome to the TicTacToe Game");
 		for(int i = 0 ;i < board.length ; i++)
 		{
 			board[i] = ' ';
 		}
 	}
 	public static void main(String[] args) {
-		System.out.println("Welcome to the TicTacToe Game");
-		TicTacToeGame tc = new TicTacToeGame();
-		tc.board();
+			board();
 	}
-
 }
