6ab029508bf429956ae7c79c11ff59c652d2d83b
Tukaram-Rathod <tukaram.p.rathod02@gmail.com>
2021-04-06 13:41:52 +0530

    Added. added choose later method for choose X or O

--- src/game/tictactoe/TicTacToeGame.java
+++ src/game/tictactoe/TicTacToeGame.java
@@ -1,16 +1,35 @@
 package game.tictactoe;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class TicTacToeGame {
-	
-	public static void  board() {
+	//created method createboard
+	public static void  createBoard() {
 		char [] board = new char[10];
-		System.out.println("Welcome to the TicTacToe Game");
+		
 		for(int i = 0 ;i < board.length ; i++)
 		{
 			board[i] = ' ';
 		}
 	}
+	//chooseplayer who isplaying 
+	public static void chooselater()
+	{
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Enter X or O");
+		char input = scanner.next().charAt(0);
+		
+		if(input == 'X'	&& input == 'x')
+			System.out.println("X");
+		else
+			System.out.println("O");
+	}
+	
+	
 	public static void main(String[] args) {
-			board();
+		System.out.println("Welcome to the TicTacToe Game");
+		createBoard();
+		chooselater();
 	}
 }
